d537152c04bfae55bf16c4805531be3a99af904a
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2025-11-30 21:02:29 -0800

    Added monster with decent pathfinding

---
+++ Monster/monsterNav.gd
@@ -0,0 +1,26 @@
+extends CharacterBody3D
+
+@onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+
+var prevPos : Vector3
+var stuckThreshold = 0.01
+
+func _unhandled_input(event: InputEvent) -> void:
+		if event.is_action_pressed("ui_accept"):
+			var random_position := Vector3.ZERO
+			random_position.x = randf_range(10.0, 60.0)
+			random_position.z = randf_range(4.0, 55.0)
+			navigation_agent_3d.set_target_position(random_position)
+
+func _physics_process(_delta: float) -> void:
+	var destination = navigation_agent_3d.get_next_path_position()
+	var local_destination = destination - global_position
+	var direction = local_destination.normalized()
+	
+	velocity = direction * 5.0
+	
+	if global_position.distance_to(prevPos) < stuckThreshold and velocity.length() > stuckThreshold:
+		velocity.y = 15.0
+	
+	prevPos = global_position
+	move_and_slide()
